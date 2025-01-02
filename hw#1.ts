3bdf5e2636cd591e177c8200cf5c8c2a613d2927
Natch <nataliach@email.cz>
2025-01-02 22:43:32 +0100

    Added hw#1

---
+++ hw#1.ts
@@ -0,0 +1,12 @@
+console.log("Синий");
+console.log("Зеленый");
+console.log("Красный");
+
+
+console.log("   *   ");
+console.log("  ***  ");
+console.log(" ***** ");
+console.log("*******");
+console.log(" ***** ");
+console.log("  ***  ");
+console.log("   *   ");
